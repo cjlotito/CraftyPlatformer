8bcf871b709a68ee273458a3ac002f71e0259d4c
Curtis Lotito <cjlotito@gmail.com>
2016-07-29 21:03:51 -0400

    Modified setBackground Function

--- Exp_LevelMaker/src/game.js
+++ Exp_LevelMaker/src/game.js
@@ -450,18 +450,23 @@ Game = {
 			i = 0;
 			while (i < sceneList.length) {
 				for (j=0; j < sceneList[i].length; j++) {
-				//[blockClass, blockColor, attr1(name), attr2(type)]
-				//Rock(  38,         10,     1,       1,      red,  'RockCover', 'Unlock', 'Right');
-				ex = j, wy = i;
-				make = sceneList[wy][ex];
-				rockClass = make[0], rockColor = make[1], rockName = make[2], rockType = make[3]; 
-				text = 'Class: ' + rockClass + ' Color: ' + rockColor + ' Name: ' + rockName + ' Type: ' + rockType;
-				document.getElementById("console").value = text;
-				if (rockColor == 0) rockColor = 'white';
-					Crafty.e('Box').color(rockColor).at(j, i);
+					//[blockClass, blockColor, attr1(name), attr2(type)]
+					//Rock(  38,         10,     1,       1,      red,  'RockCover', 'Unlock', 'Right');
+					ex = j, wy = i, make = sceneList[wy][ex], rockColor = make[1]; 
+					
+					rockClass = make[0], rockColor = make[1], rockName = make[2], rockType = make[3]; 
+					//text = 'Class: ' + rockClass + ' Color: ' + rockColor + ' Name: ' + rockName + ' Type: ' + rockType;
+					document.getElementById("console").value = text;
+					if (rockColor == 0) {
+						rockColor = playerColor;
+						Crafty.e('Box').color(rockColor);
+						make[1] = rockColor;
+					}
+					i++;
 				}
-				i++;
 			}
+		Game.printArray();
+		Game.setBackground(document.getElementById('UserCode').value);
 	},
 	reSkin: function() {
 		i = 0;
