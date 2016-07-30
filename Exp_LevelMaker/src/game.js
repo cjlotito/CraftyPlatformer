65683691af84139affa09bd115f76c449ccacf1f
Curtis Lotito <cjlotito@gmail.com>
2016-07-29 21:38:10 -0400

    Update game.js

--- Exp_LevelMaker/src/game.js
+++ Exp_LevelMaker/src/game.js
@@ -444,21 +444,24 @@ Game = {
 		text = 'Paste is: ' + paste;
 		document.getElementById("console").value = text;
 	},**/
-	setBackground: function () {
-		i = 0;
-		while (i < sceneList.length) {
-			for (j=0; j < sceneList[i].length; j++) {
-				ex = j, wy = i, make = sceneList[wy][ex], rockColor = make[1]; 
-				if (rockColor == "0") {
-					rockColor = playerColor;
-					Crafty.e('Box').color(rockColor);
-					make[1] = rockColor;
+	setBackground: function (hold) {
+		list = JSON.parse(hold);
+		sceneList = list;
+			i = 0;
+			while (i < sceneList.length) {
+				for (j=0; j < sceneList[i].length; j++) {
+				//[blockClass, blockColor, attr1(name), attr2(type)]
+				//Rock(  38,         10,     1,       1,      red,  'RockCover', 'Unlock', 'Right');
+				ex = j, wy = i;
+				make = sceneList[wy][ex];
+				rockClass = make[0], rockColor = make[1], rockName = make[2], rockType = make[3]; 
+				text = 'Class: ' + rockClass + ' Color: ' + rockColor + ' Name: ' + rockName + ' Type: ' + rockType;
+				document.getElementById("console").value = text;
+				if (rockColor == 0) rockColor = 'white';
+					Crafty.e('Box').color(rockColor).at(j, i);
 				}
+				i++;
 			}
-			i++;
-		}
-		Game.printArray();
-		Game.setBackground(document.getElementById('UserCode').value);
 	},
 	reSkin: function() {
 		i = 0;
