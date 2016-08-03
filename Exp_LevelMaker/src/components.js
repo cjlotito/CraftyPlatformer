50d631e4bad48f02dba40eb248da1ed14f34d1e2
Curtis Lotito <cjlotito@gmail.com>
2016-08-03 17:55:51 -0400

    Removed UpScroll

--- Exp_LevelMaker/src/components.js
+++ Exp_LevelMaker/src/components.js
@@ -108,27 +108,6 @@ Crafty.c('Scroll', {
 	},
 });
 
-Crafty.c('UpScroll', {
-	init: function() {
-		player = this; 
-		clicked = false;
-		speed = 7;
-		keys = {UP_ARROW: -90, DOWN_ARROW: 90};
-		this.requires('Actor, Color, Multiway, Collision, Mouse')
-			.attr({x: 0, y: 35, w: 20, h: 35, z: 3})
-			.color('blue')
-			.multiway(speed, keys)
-			.onHit('Stop', function(object) {
-				entity = object[0].obj;
-				if (this.x > entity.x) {
-					this.x = entity.x + entity.w;
-				} else if (this.x < entity.x) {
-					this.x = entity.x - this.w;
-				}
-			});
-	}
-});
-
 //Costume/Pause Menu
 Crafty.c('Menu', {
 	init: function() {
@@ -165,7 +144,7 @@ Crafty.c('Blank', {
 Crafty.c('Stop', {
 	init: function() {		
 		this.requires('Actor, Color, Collision, Mouse')     
-			.attr({x: 120, y: 300, w: Game.tileW, h: Game.tileH, z:10})
+			.attr({x: 120, y: 300, w: 35, h: 20})
 			.color('gray');
 	}
 });
