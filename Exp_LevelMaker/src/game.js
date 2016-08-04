837d30d45be4119191232a1c573df0827b00aa28
Curtis Lotito <cjlotito@gmail.com>
2016-08-03 22:19:46 -0400

    Update game.js

--- Exp_LevelMaker/src/game.js
+++ Exp_LevelMaker/src/game.js
@@ -545,9 +545,9 @@ Game = {
 		playerColor = sel;
 	},
 	setGrid: function(x,y, gx, gy) {
-		console.log("setgrid started. Received: " + x + ", " + y + ", " + gx + ", " + gy);
 		tileW = x, tileH = y, Game.map_grid.tile.width = x, Game.map_grid.tile.height = y;
 		gridW = gx, gridH = gy, Game.map_grid.width = gx, Game.map_grid.height = gy; 
+		console.log("setgrid started. Received: " + Game.map_grid.tile.width + ", " + Game.map_grid.tile.height + ", " + Game.map_grid.width + ", " + Game.map_grid.height);
 		Game.start();
 	},
 	// Initialize and start our game
