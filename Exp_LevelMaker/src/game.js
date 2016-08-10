019979308a66980e18b0a319383d58d5fc0b6c10
Curtis Lotito <cjlotito@gmail.com>
2016-08-09 22:30:40 -0400

    Added X/Y Setter

--- Exp_LevelMaker/src/game.js
+++ Exp_LevelMaker/src/game.js
@@ -561,7 +561,7 @@ Game = {
 			sceneRow[i] = sceneBlank;
 			for (j = 0; j < Game.map_grid.height-1; j++) { 
 				sceneList[j] = sceneRow;
-				//sceneList[j][i][4] = j + ',' + i;
+				sceneList[j][i][4] = j + ',' + i;
 				console.log(sceneList[j][i]);
 			}
 		}
