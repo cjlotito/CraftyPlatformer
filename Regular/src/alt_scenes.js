6c8ac387e410e50113d1b6363a3903f95d45f0e4
Curtis Lotito <cjlotito@gmail.com>
2016-08-19 18:27:44 -0400

    Commiting Changes From File

--- Regular/src/alt_scenes.js
+++ Regular/src/alt_scenes.js
@@ -23,7 +23,7 @@ var sceneSet = function(list) {
 					if (rockColor == 'clear') {
 						if (debug == false) Crafty.e('RockCover').at(ex, wy).removeComponent('Color').attr({name: rockName, type: rockType});
 					} else {
-						if (debug == false) Crafty.e('RockCover').at(ex, wy).color(rockColor).attr({name: rockName, type: rockType});
+						if (debug == false) Crafty.e('RockCover').at(ex, wy).addComponent(rockColor).attr({w: tileW, h: tileH, name: rockName, type: rockType});
 					}
 				} else {
 					Crafty.e('TinyGround').at(ex + 0.15, wy);
@@ -32,7 +32,8 @@ var sceneSet = function(list) {
 					if (rockColor == 'clear') {
 						if (debug == false) Crafty.e('RockCover').at(ex, wy - 0.05).removeComponent('Color').attr({name: rockName, type: rockType});
 					} else {
-						if (debug == false) Crafty.e('RockCover').at(ex, wy - 0.05).color(rockColor).attr({name: rockName, type: rockType});
+						//if (debug == false) Crafty.e('RockCover').at(ex, wy - 0.05).color(rockColor).attr({name: rockName, type: rockType});
+						if (debug == false) Crafty.e('RockCover').at(ex, wy - 0.05).addComponent(rockColor).attr({w: tileW, h: tileH, name: rockName, type: rockType});
 					}
 				}	
 			} else if (rockClass === 'ColorCover') {
@@ -43,7 +44,8 @@ var sceneSet = function(list) {
 					if (rockColor == 'clear') {
 						if (debug == false) Crafty.e('ColorCover').at(ex, wy - 0.05).removeComponent('Color').attr({name: rockName, type: rockType});
 					} else {
-						if (debug == false) Crafty.e('ColorCover').at(ex, wy - 0.05).color(rockColor).attr({name: rockName, type: rockType});
+						//if (debug == false) Crafty.e('ColorCover').at(ex, wy - 0.05).color(rockColor).attr({name: rockName, type: rockType});
+						if (debug == false) Crafty.e('ColorCover').at(ex, wy - 0.05).addComponent(rockColor).attr({w: tileW, h: tileH, name: rockName, type: rockType});
 					}
 				} else if (rockName === 'AntiColorZone') {
 					Crafty.e('TinyGround').at(ex + 0.15, wy + 0.7);
@@ -52,16 +54,39 @@ var sceneSet = function(list) {
 					if (rockColor == 'clear') {
 						if (debug == false) Crafty.e('ColorCover').at(ex, wy).removeComponent('Color').attr({name: rockName, type: rockType});
 					} else {
-						if (debug == false) Crafty.e('ColorCover').at(ex, wy).color(rockColor).attr({name: rockName, type: rockType});
+						//if (debug == false) Crafty.e('ColorCover').at(ex, wy).color(rockColor).attr({name: rockName, type: rockType});
+						if (debug == false) Crafty.e('ColorCover').at(ex, wy).addComponent(rockColor).attr({w: tileW, h: tileH, name: rockName, type: rockType});
 					}
 				}
 			} else if (rockClass == 'back') {
 				Crafty.e('Background').at(ex, wy).color(rockColor);	
+			} else if (rockClass == 'Door') {
+				if (rockColor == 'clear') {
+					Crafty.e(rockClass).at(ex, wy - 0.05).removeComponent('Color').attr({h:tileH*3, w: tileW, name: rockName, type: rockType});
+				} else {
+					//Crafty.e(rockClass).at(ex, wy - 0.05).color(rockColor).attr({name: rockName, type: rockType});
+					Crafty.e(rockClass).at(ex, wy - 0.05).addComponent(rockColor).attr({h:tileH*3, w: tileW, name: rockName, type: rockType});
+				}
+			} else if (rockClass == 'Elevator') {
+				if (rockColor == 'clear') {
+					Crafty.e(rockClass).at(ex, wy - 0.05).removeComponent('Color').attr({w: tileW*2, h:tileH*4, name: rockName, type: rockType});
+				} else {
+					//Crafty.e(rockClass).at(ex, wy - 0.05).color(rockColor).attr({name: rockName, type: rockType});
+					Crafty.e(rockClass).at(ex, wy - 0.05).addComponent(rockColor).attr({w: tileW*2, h:tileH*4, name: rockName, type: rockType});
+				}
+			} else if (rockClass == 'Wall') {
+				if (rockColor == 'clear') {
+					Crafty.e(rockClass).at(ex, wy - 0.05).removeComponent('Color').attr({h:tileH*5, w: tileW, name: rockName, type: rockType});
+				} else {
+					//Crafty.e(rockClass).at(ex, wy - 0.05).color(rockColor).attr({name: rockName, type: rockType});
+					Crafty.e(rockClass).at(ex, wy - 0.05).addComponent(rockColor).attr({h:tileH*5, w: tileW, name: rockName, type: rockType});
+				}
 			} else {
 				if (rockColor == 'clear') {
 					Crafty.e(rockClass).at(ex, wy - 0.05).removeComponent('Color').attr({name: rockName, type: rockType});
 				} else {
-					Crafty.e(rockClass).at(ex, wy - 0.05).color(rockColor).attr({name: rockName, type: rockType});
+					//Crafty.e(rockClass).at(ex, wy - 0.05).color(rockColor).attr({name: rockName, type: rockType});
+					Crafty.e(rockClass).at(ex, wy - 0.05).addComponent(rockColor).attr({w: tileW, h: tileH, name: rockName, type: rockType});
 				}
 			}
 			//END COPY
@@ -90,6 +115,9 @@ Crafty.scene('Onward', function(toPrint) {
 			}
 		}
 	}
+	
+	//Tile Sprite Test
+	//Crafty.e('RockCover').at(1, 1).color('red').addComponent('gray');
 	
 	//Start Button
 	var pauseBtn = Crafty.e('Blank').at(0, 0).bind('KeyDown', this.start_game);
@@ -126,7 +154,6 @@ Crafty.scene('LevelTest', function() {
 	});
 	//END FIX	
 	
-	
 	sceneSet(specialSceneList);
 	console.log('Player Start point is: ' + checkPoint[0] + ',' + checkPoint[1]);
 	player.at(checkPoint[0], checkPoint[1]);
@@ -136,12 +163,24 @@ Crafty.scene('LevelTest', function() {
 Crafty.scene('Level', function() {
 	if (fresh) {
 		//currentSceneList = Game.copyArr(sceneList1);
-		currentSceneList = myLevels[startPoint-1];
+		
+		currentSceneList = Game.copyArr(myLevels[startPoint-1]);
+		
+		//currentSceneList = myLevels.slice(startPoint-1,startPoint)[0];
 		//console.log("After: " + currentSceneList);
 		numbers = currentSceneList[currentSceneList.length-1][currentSceneList[0].length][3].split(','), ex = numbers[0], wy = numbers[1];
 		checkPoint[0] = ex, checkPoint[1] = wy;
 		fresh = false;
 	}
+
+	listX = currentSceneList.length-1, listY = currentSceneList[0].length;
+	//tilesize-Set
+	tileNumbers = currentSceneList[listX][listY][2].split(','), tileW = parseInt(tileNumbers[0]), tileH = parseInt(tileNumbers[1]);
+	Game.map_grid.tile.width = tileW, Game.map_grid.tile.height = tileH;
+	//gridsize-Set
+	gridNumbers = currentSceneList[listX][listY][1].split(','), gridW = parseInt(gridNumbers[0]), gridH = parseInt(gridNumbers[1]);
+	Game.map_grid.width = gridW, Game.map_grid.height = gridH;
+	
 	console.log('Tiles: ' + tileW + ', ' + tileH);
 	console.log('Grid: ' + gridW + ', ' + gridH);
 	
